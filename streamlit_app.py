fddc70d5ca4e62ff7804c80d2515055fc2979e17
Santosh Ransubhe <santoshtalks@gmail.com>
2022-09-28 16:54:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,8 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('\N{flexed biceps}Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spincach & Rocket Smoothie')
+streamlit.text('\N{leafy green}Kale, Spincach & Rocket Smoothie')
 streamlit.text('\N{chicken}Hard-Boiled Free Range Egg')
 streamlit.text('\N{avocado} Avocado Toast')
 
-streamlit.header('\N{banana} \N{red apple} Build your own fruit smoothie \N{banana}')
+streamlit.header('\N{banana} \N{red apple} Build your own fruit smoothie \N{pineapple} \N{pear}')
